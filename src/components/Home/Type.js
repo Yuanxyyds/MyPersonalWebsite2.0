14227665f2e54aa963f5bb09d4ef79e89c5dbca8
Yuanxyyds <liuhongyuan.liu@mail.utoronto.ca>
2023-07-23 18:59:52 -0400

    feat: initialize the project

---
+++ src/components/Home/Type.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Student at University of Toronto",
+          "Cloud and Mobile App Developer",
+          "Web Developer",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
