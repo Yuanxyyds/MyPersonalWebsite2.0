4a94a32dd3177a6d2f2c9f3ab00de850357ef19b
Yuanxyyds <liuhongyuan.liu@mail.utoronto.ca>
2023-07-24 03:32:35 -0400

    feat: add project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,95 +1,135 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import campusEats from "../../Assets/Projects/campusEats.png";
+import GreatLakes from "../../Assets/Projects/Great-lakes.png";
+import DSWsb from "../../Assets/Projects/DS-web.png";
+import PersonalWeb from "../../Assets/Projects/personal-web.png"
+import Button from "react-bootstrap/Button";
+import {BsGithub} from "react-icons/bs";
+import {CgWebsite} from "react-icons/cg";
 
 function Projects() {
-  return (
-    <Container fluid className="project-section">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="blue">Works </strong>
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
+    return (
+        <Container fluid className="project-section">
+            <Particle/>
+            <Container>
+                <h1 className="project-heading">
+                    My Recent <strong className="blue">Works </strong>
+                </h1>
+                <p style={{color: "white"}}>
+                    Here are a few projects I've worked on recently.
+                </p>
+                <Row style={{justifyContent: "center", paddingBottom: "10px"}}>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={campusEats}
+                            title="Campus Eats Mobile App"
+                            description="As an extension to the UofT FoodTruck Project, we have created a comprehensive mobile food delivery app, catering to users, restaurants, and drivers, with the goal of enhancing campus food services. The app was developed using Flutter and Firebase backend."
+                            action={
+                                <>
+                                    <Button variant="primary" href={"https://github.com/Yuanxyyds/FoodTruckMobile"}
+                                            target="_blank">
+                                        <CgWebsite/> &nbsp; Project Demo
+                                    </Button>
+                                </>}
+                        />
+                    </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={PersonalWeb}
+                            title="Personal Website"
+                            description="My personal website build with Next.js and Tailwind Css (version 1.0), React App (version 2.0). Both website includes detailed explanations (including videos) of all my projects."
+                            action={
+                                <>
+                                    <Button variant="primary" href={"https://github.com/Yuanxyyds/MyPersonalWebsite2.0"}
+                                            target="_blank">
+                                        <BsGithub/> &nbsp; Version 2.0
+                                    </Button>
+                                    {"\n"}
+                                    {"\n"}
+                                    <Button
+                                        variant="primary"
+                                        href={"my-personal-website2-0.vercel.app"}
+                                        target="_blank"
+                                        style={{marginLeft: "10px"}}>
+                                        <CgWebsite/> &nbsp; Website
+                                    </Button>
+                                    {"\n"}
+                                    {"\n"}
+                                    <Button variant="primary" href={"https://github.com/Yuanxyyds/MyPersonalWebsite"}
+                                            target="_blank" style={{marginTop: "10px"}}>
+                                        <BsGithub/> &nbsp; Version 1.0
+                                    </Button>
+                                    {"\n"}
+                                    {"\n"}
+                                    <Button
+                                        variant="primary"
+                                        href={"https://liustev6.ca"}
+                                        target="_blank"
+                                        style={{marginLeft: "10px", marginTop: "10px"}}>
+                                        <CgWebsite/> &nbsp; Website
+                                    </Button>
+                                </>}
+                        />
+                    </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={GreatLakes}
+                            title="Analyze Great Lakes Pollution"
+                            description="This project investigates Great Lakes water quality, focusing on phosphorus levels. Utilizing data from The Canadian Open Data Portal and web-scraped city data. Delivered with time series plots, boxplots, and interactive maps for visualization. "
+                            action={
+                                <>
+                                    <Button variant="primary"
+                                            href={"https://github.com/Yuanxyyds/Analyzing-Great-Lakes-Polution"}
+                                            target="_blank">
+                                        <BsGithub/> &nbsp; Github
+                                    </Button>
+                                    {"\n"}
+                                    {"\n"}
+                                    <Button
+                                        variant="primary"
+                                        href={"https://yuanxyyds.github.io/Analyzing-Great-Lakes-Polution/"}
+                                        target="_blank"
+                                        style={{marginLeft: "10px"}}>
+                                        <CgWebsite/> &nbsp; Website
+                                    </Button>
+                                </>}
+                        />
+                    </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
-        </Row>
-      </Container>
-    </Container>
-  );
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={DSWsb}
+                            title="Data Science Projects Collection Website"
+                            description="My data science project showcase website written by RMarkdown. Including projects: Predict NBA Player's NBA2K Rating; NLP in Identifying Clash Royale Players' Feeling; Data Visualization for Bike Thefts in Toronto; Analyze Life Expectation versus Alcohol Consumption."
+                            action={
+                                <>
+                                    <Button variant="primary"
+                                            href={"https://github.com/Yuanxyyds/Data-Science-Projects"}
+                                            target="_blank">
+                                        <BsGithub/> &nbsp; Github
+                                    </Button>
+                                    {"\n"}
+                                    {"\n"}
+                                    <Button
+                                        variant="primary"
+                                        href={"https://yuanxyyds.github.io/Data-Science-Projects/"}
+                                        target="_blank"
+                                        style={{marginLeft: "10px"}}>
+                                        <CgWebsite/> &nbsp; Website
+                                    </Button>
+                                </>}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
 }
 
 export default Projects;
