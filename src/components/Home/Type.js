a634ee0133a84109c198fab035777bc11b948583
Yuanxyyds <liuhongyuan.liu@mail.utoronto.ca>
2023-07-24 19:36:00 -0400

    feat: edit website title

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,27 +1,23 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
-import Container from "react-bootstrap/Container";
 
 function Type() {
     return (
-        <Container fluid>
-            <div className="typewriter-wrapper-home">
-                <Typewriter
-                    options={{
-                        strings: [
-                            "Student at University of Toronto",
-                            "Cloud and Mobile App Developer",
-                            "Web Developer",
-                        ],
-                        autoStart: true,
-                        loop: true,
-                        deleteSpeed: 50,
-                    }}
-                />
-            </div>
+        <div className="typewriter-wrapper-home">
+            <Typewriter
+                options={{
+                    strings: [
+                        "Student at University of Toronto",
+                        "Cloud and Mobile App Developer",
+                        "Web Developer",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 50,
+                }}
+            />
+        </div>
 
-
-        </Container>
 
     );
 }
