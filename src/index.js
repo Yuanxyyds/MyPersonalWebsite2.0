331335654c8323a843f0c490527c82e30733643f
Yuanxyyds <liuhongyuan.liu@mail.utoronto.ca>
2025-07-12 22:39:40 -0700

    update resume section

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import "./style/style.css";
 import "./style/Footer.css";
 import "./style/Pre.css"
 import App from "./pages/App";
